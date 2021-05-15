277f030f99e059588e30c7534c415192aec15ef6
boo105 <boo10500@naver.com>
2021-05-15 14:22:28 +0900

    DB 연결 및 유닛 테스트

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body,Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  
   @Get()
-  getHello(): string {
+  getHello(): string 
+  {
     return this.appService.getHello();
+  }
+
+  @Post("/test")
+  showTemperature(@Body() data)
+  {
+    return "CDS : " + data.CDS + " TEM : " + data.TEM + " data.HUM";
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,9 @@ export class AppService {
   getHello(): string {
     return 'api-plantsmartfarm 작동 성공!!!';
   }
+
+  showTemperature(data)
+  {
+    
+  }
 }
