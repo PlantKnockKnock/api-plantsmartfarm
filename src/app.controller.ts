27a3992825096f256a9448db32a91100ba1618d9
boo105 <boo10500@naver.com>
2021-05-15 14:34:40 +0900

    Revert "DB 연결 및 유닛 테스트"
    This reverts commit 277f030f99e059588e30c7534c415192aec15ef6.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,12 @@
-import { Body,Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  
-  @Get()
-  getHello(): string 
-  {
-    return this.appService.getHello();
-  }
 
-  @Post("/test")
-  showTemperature(@Body() data)
-  {
-    return "CDS : " + data.CDS + " TEM : " + data.TEM + " data.HUM";
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
