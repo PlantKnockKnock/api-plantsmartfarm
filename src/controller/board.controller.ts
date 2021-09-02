f96a8eb471effe839e580ad3794fbeb10fcde0f4
boo105 <boo10500@naver.com>
2021-09-02 18:30:34 +0900

    스펠링 틀린거 고침...

--- src/controller/board.controller.ts
+++ src/controller/board.controller.ts
@@ -11,7 +11,7 @@ export class BoardController {
   async getBoardList () {
     const posts = await this.boardService.findAll();
     return {
-      sucess : true,
+      success : true,
       statusCode : 200,
       posts : posts
     };
